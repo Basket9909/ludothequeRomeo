b73ca2916a24cb8cc20378866f7418620ab97272
Romeo <romeo.wilmart@hotmail.fr>
2021-09-17 14:43:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Ludotheque Romeo</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li class="grandLi"><h2 class="rotateH2">Romeo's ludotheque</h2></li>
+            <li><a href="test">Test</a></li>
+            <li><a href="test">Test</a></li>
+            <li><a href="test">Test</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
